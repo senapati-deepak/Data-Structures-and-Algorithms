84f272460036d7eec6a08070bed7db9722a54853
Deepak345 <senapati.dipak97@gmail.com>
2018-08-26 23:14:51 +0530

    Week 4

--- Algorithmic Toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ Algorithmic Toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -4,6 +4,15 @@ import sys
 def binary_search(a, x):
     left, right = 0, len(a)
     # write your code here
+    while left < right:
+        mid = (left+right)//2
+        if x == a[mid]:
+            return mid
+        elif x > a[mid]:
+            left = mid+1
+        else:
+            right = mid
+    return -1
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +28,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
--- Algorithmic Toolbox/week4_divide_and_conquer/2_majority_element/majority_element.py
+++ Algorithmic Toolbox/week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,13 +1,32 @@
 # Uses python3
 import sys
 
+
 def get_majority_element(a, left, right):
     if left == right:
         return -1
     if left + 1 == right:
         return a[left]
-    #write your code here
+
+    leftMajority = get_majority_element(a, left, (left + right - 1) // 2 + 1)
+    rightMajority = get_majority_element(a, (left + right - 1) // 2 + 1, right)
+
+    left_count = 0
+    for i in range(left, right):
+        if a[i] == leftMajority:
+            left_count += 1
+    if left_count > (right - left) // 2:
+        return leftMajority
+
+    right_count = 0
+    for i in range(left, right):
+        if a[i] == rightMajority:
+            right_count += 1
+    if right_count > (right - left) // 2:
+        return rightMajority
+
     return -1
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
